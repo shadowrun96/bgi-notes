9b2dbb2193656dc91a225518fb263a5e22153f09
george <118578531+shadowrun96@users.noreply.github.com>
2024-06-10 17:07:35 +0300

    Update inp.cpp

--- inp.cpp
+++ inp.cpp
@@ -1,3 +1,5 @@
+// getinp.cpp
+
 #include <iostream.h>
 #include <iomanip.h>
 
@@ -5,7 +7,12 @@ void main() {
 	char inp[255];
 	int inplen = 0;
 
+	int tokenfound = 0;
+	int spcfound = 0;
+
 	for (int k=0;k<254;k++) inp[k]=0;
+
+
 	cin.get(inp, 255);
 
 	for (int l=0;l<254;l++) {
@@ -14,5 +21,26 @@ void main() {
 
 	cout << "buffer:" << inp << endl;
 	cout << "length:" << inplen << endl;
+
+
+	// check to see if there are tokens separated by space
+	// larger than one character. If found, exit with error.
+
+	for (int t=0;t<254;t++)
+	{
+
+	  // a_b
+	  //
+
+	  if (inp[t]==0) { break;}
+
+	  if (inp[t]!=' ' && tokenfound==0)
+	     { cout << t << "token found" << endl;tokenfound=1;continue;}
+	  if (inp[t]!=' ' && tokenfound==1)
+	     { cout << t << "token>1 found" << endl;break;}
+	  if (inp[t]==' ')
+	     {cout << t << "spc found" << endl; tokenfound=0; spcfound=1;continue;}
+
+	}
 
 }
